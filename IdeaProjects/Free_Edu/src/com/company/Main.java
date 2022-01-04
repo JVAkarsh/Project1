6d5388c9470bca08032e9df70e7aacc104672d1a
JVAkarsh <masterakarsh@gmail.com>
2022-01-04 21:47:09 +0530

    Prime within 100

---
+++ IdeaProjects/Free_Edu/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int num = 2; num <= 100; num++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime == true) {
+                System.out.println(num);
+            }
+        }
+    }
+}
